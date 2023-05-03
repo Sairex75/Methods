ae5fea5811448f5e1f40fee5fc9e52ff3d112649
Sairex75 <ghostitsky@gmail.com>
2023-05-03 13:13:14 +0300

    Initial commit

---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            RecieveWords(SplitText(Console.ReadLine()));
+        }
+        static string[] SplitText(string text)
+        {
+            string[] arrayForSplit = text.Split(" ");
+            return arrayForSplit;
+        }
+
+        static void RecieveWords(string[] printWords)
+        {
+            foreach (string v in printWords)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
---
+++ ReplaceSentence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReplaceSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите предложение: ");
+            Reverse(SplitText(Console.ReadLine()));
+           
+        }
+        static string[] SplitText(string text)
+        {
+            string[] arrayForSplit = text.Split(" ");
+            return arrayForSplit;
+        }
+
+        static void Reverse(string[] text)
+        {
+            string word;
+            string newWord = null;
+            for (int i = text.Length - 1; i > -1; i--)
+            {
+                word = text[i];
+                newWord += word + " ";
+            }
+            Console.WriteLine(newWord);
+        }
+    }
+}
+
